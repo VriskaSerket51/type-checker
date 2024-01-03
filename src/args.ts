1cdc3cad15549de943e37e748f8bd17e99df63da
vriskaserket51 <serketvriska51@gmail.com>
2024-01-04 01:27:32 +0900

    Add help args

--- src/args.ts
+++ src/args.ts
@@ -1,13 +1,23 @@
 import { parse } from "ts-command-line-args";
 
 export interface IArguments {
-  src: string;
-  out: string;
-  strict: boolean
+    src: string;
+    out: string;
+    strict: boolean;
 }
 
 export const args = parse<IArguments>({
-  src: String,
-  out: String,
-  strict: Boolean
+    src: {
+        type: String,
+        description: "Input directory which contains .ts files",
+    },
+    out: {
+        type: String,
+        description: "Output directory which sanitizer script will be saved to",
+    },
+    strict: {
+        type: Boolean,
+        description:
+            "Whether use strict mode. In strict mode, script does not check string value can be converted to number or boolean",
+    },
 });
